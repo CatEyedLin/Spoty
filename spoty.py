ebb49d13dd8d65716fd3fbe8917b96967bd36e06
CatEyedLin <cat.eyed.link64@gmail.com>
2023-05-29 21:57:30 -0500

    test site

--- spoty.py
+++ spoty.py
@@ -6,3 +6,31 @@ client_ID = env.readline().strip("\n")
 client_secret = env.readline().strip("\n")
 
 #https://api.spotify.com/v1/me/player/pause
+
+AUTH_URL = 'https://accounts.spotify.com/api/token'
+BASE_URL = 'https://api.spotify.com/v1/'
+
+# POST
+auth_response = requests.post(AUTH_URL, {
+    'grant_type': 'client_credentials',
+    'client_id': client_ID,
+    'client_secret': client_secret,
+})
+
+# convert the response to JSON
+auth_response_data = auth_response.json()
+
+# save the access token
+access_token = auth_response_data['access_token']
+
+headers = {
+    'Authorization': 'Bearer {token}'.format(token=access_token)
+}
+
+artist_id = '33tFkBLsl6f8TjKkV0uF0C'
+
+# pull all artists albums
+r = requests.get(BASE_URL + 'artists/' + artist_id + '/albums', 
+                 headers=headers, 
+                 params={'include_groups': 'album', 'limit': 50})
+d = r.json()
